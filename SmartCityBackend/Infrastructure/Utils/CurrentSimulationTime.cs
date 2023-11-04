edf0963bbe0aad4e9442f16c234d84b575ba7a39
zzunic <zzunic@playnirvana.com>
2023-11-05 00:32:17 +0100

    CurrentSimulationTime Util

---
+++ SmartCityBackend/Infrastructure/Utils/CurrentSimulationTime.cs
@@ -0,0 +1,25 @@
+﻿namespace SmartCityBackend.Infrastructure.Utils;
+
+public static class CurrentSimulationTime
+{
+    
+    public static (int simulationHours, int simulationMinutes)? GetCurrentSimulationTime()
+    {
+        // Get the current real-world time
+        DateTimeOffset now = DateTimeOffset.UtcNow;
+        
+        var simulationHours = now.Minute % 30;
+        var simulationMinutes = now.Second % 30;
+        
+        if (simulationHours >= 24 && simulationHours < 30)
+        {
+            return null;
+        }
+        
+        // Return the simulation time
+        return (simulationHours, simulationMinutes);
+    }
+
+
+
+}
